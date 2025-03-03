edee5666440566edf09dccf460972b750e4c0e08
Fellipe Cavalcante <fc.g.hub@f00cavalcante.com>
2025-03-03 11:45:17 -0300

    Questão 02: Fibonacci
    Refere-se ao ajuste da função responsávelo por efetuar o cálculo da sequência Fibonacci, que será o número inteiro enviado no parâmetro da função.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from arquivos.caminho_dos_arquivos import CaminhoDosArquivos
 
 from questao_01.resposta_questao_01 import questao_um_retorno_soma
+from questao_02.resposta_questao_02 import sequencia_fibonacci
 
 if __name__ == '__main__':
     tralha_para_ficar_bonito_no_console: str = '##########'
@@ -10,7 +11,21 @@ if __name__ == '__main__':
 
     print(f'\n{tralha_para_ficar_bonito_no_console}')
     print(f'Questão 01 - Soma')
-    print(f'Nesta questão, foi passado um trecho de código, com o intuito de validar qual o resultado gerado, desta forma, efetuei a replicação em Python, para obter o resultado, e exibí-lo ao efetuar a invocação da função. Desta forma, '
-          f'segue abaixo o resultado.')
+    print(
+        f'Nesta questão, foi passado um trecho de código, com o intuito de validar qual o resultado gerado, desta forma, efetuei a replicação em Python, para obter o resultado, e exibí-lo ao efetuar a invocação da função. Desta forma, '
+        f'segue abaixo o resultado.'
+    )
     print(f'Resultao da soma: ', end='')
     questao_um_retorno_soma()
+
+    print(f'\n{tralha_para_ficar_bonito_no_console}')
+    print(f'Questão 02 - Sequência Fibonacci')
+    print(
+        f'Nesta questão, foi aplicado a lógica da sequência Fibonacci, a qual, resumidamente, consiste em efetuar a soma numérica de determinada sequência repetidas vezes até seu total numérico, isto é: \n'
+        f'A sequência Fibonacci de 10 consiste em sua soma sequêncial de 0 até 10, pelos seus números que os compõem, ou seja: '
+        f'0 + 1 + 2 [...] 10'
+    )
+    print(f'Assim sendo, criquei a função "sequencia_fibonacci", a qual receberá como parâmetro a sequência Fibonacci que deseja obter. Por ser um ponto apenas para validar a lógica de programação, irei inserir o numeral 10 fixamente ao '
+          f'invocá-la.')
+    print(f'Desta forma, conseguimos validar que o Fibonacci(10) possui [...] ', end='')
+    sequencia_fibonacci(10)
--- questao_02/resposta_questao_02.py
+++ questao_02/resposta_questao_02.py
@@ -1,13 +1,10 @@
 # Fibonacci
 
-sequencia_fibonacci = int(input('Informe o número de sequências Fibonacci que deseja calcular: '))
+def sequencia_fibonacci(numero_fibocacci: int):
+    finocacci: list = [0, 1]  # inicialização da sequência Fibonacci
 
-finocacci: list = [0, 1]
-
-
-for number in range(sequencia_fibonacci):
-
-     continuacao_fibonacci = finocacci[-1] + finocacci[-2]
-     finocacci.append(continuacao_fibonacci)
-
-print(f'O resultado final: {continuacao_fibonacci}')
+    for number in range(numero_fibocacci):
+        continuacao_fibonacci = finocacci[-1] + finocacci[-2]   # neste processo, o índice -1 será a garantia que sempre será validado o último item da lista, que por sua vez, somará com o penúltimo número
+        finocacci.append(continuacao_fibonacci)                 # após, a soma em questão, será adicionada à lista através do método append(), atualizando então o último índice da listagem, e garantindo a repetição da linha anterior
+                                                                # com isto, existe a garanti que o laço será repetido até que toda a lista seja populada com a númeração Fibonacci a ser validada
+    print(f'O resultado final: {continuacao_fibonacci}')
